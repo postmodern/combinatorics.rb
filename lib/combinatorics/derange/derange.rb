23dbc458bb16b1da5fa28047d8ba6c9fb9b7d2a8
duper <super@alien.localdomain>
2010-12-25 20:05:32 -0500

    Wrote some YARD-style documentation in derange.rb Added derange method to Combinatorics::Derange

--- lib/combinatorics/derange/derange.rb
+++ lib/combinatorics/derange/derange.rb
@@ -1,10 +1,33 @@
 require 'combinatorics/choose'
+require 'combinatorics/list_comprehension'
 
 module Combinatorics
   module Derange
     include Choose
 
-    def cardinality(n)
+    # @param [Array] a sequence to output derangements for
+    # @return [Array] set of the derangements for input sequence
+    # @example derange([1,2,3])
+    # @see http://en.wikipedia.org/wiki/Derangements
+    # @see Array#comprehension 
+    def derange(a)
+      c = []
+
+      for i in 0 .. a.size - 1
+        c << a - [a[i]]
+      end
+
+      # @todo wouldn't it be better to yield Enumerators for combinatorics 
+      #   methods that do set generation? (i.e. power_set, choose, derange, etc.)
+      #   so the ronin-fuzzer doesn't have to wait on the computation..
+      c.comprehension.to_a
+    end
+
+    # @param Fixnum length of sequence 
+    # @return cardinality of derangements set 
+    # @example subfactorial([1,2,3].size)
+    # @see http://mathworld.wolfram.com/Subfactorial.html
+    def subfactorial(n)
       add, sum = false, 0
 
       return sum if n.zero?
@@ -20,8 +43,22 @@ module Combinatorics
       (1 - sum).abs.to_i
     end
 
+    def cardinality(n)
+      subfactorial(n)
+    end
+
     def D(n) # discrete math notation
-      cardinality(n)
+      subfactorial(n)
+    end
+
+    # @todo: define operators for combinatorics functions? i.e. *** represents
+    #   partial_factorial, n! represents factorial and !n subfactorial..
+    def partial_factorial(j, k)
+      r = j
+
+      (j -= 1).downto(k){|n| r *= n}
+     
+      r
     end
 
     def get_fractions(n)
@@ -30,14 +67,6 @@ module Combinatorics
       n.downto(1){|x| a << partial_factorial(n, x)}
 
       a
-    end
-
-    def partial_factorial(j, k)
-      r = j
-
-      (j -= 1).downto(k){|n| r *= n}
-     
-      r
     end
   end
 end
