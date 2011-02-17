24aca6455508b1eec01de790955cfc24924b0c3e
duper <super@manson.vistech.net>
2011-02-16 20:16:27 -0500

    (same as before)

---
+++ spec/permute/array_spec.rb
@@ -0,0 +1,9 @@
+require 'spec_helper'
+require 'combinatorics/permute/array_extension'
+require 'permute/mixin_examples'
+
+describe Array do
+  subject { Array }
+
+  it_should_behave_like 'Combinatorics::Permute::Mixin'
+end
