ad66622b38d1de4b2955f5006c06a0199ce399dc
duper <super@manson.vistech.net>
2011-03-07 04:46:31 -0500

    Self-explanatory..

---
+++ lib/combinatorics/choose/cardinality.rb
@@ -0,0 +1,78 @@
+# @author duper <super@manson.vistech.net>
+
+require 'combinatorics/extensions/math'
+
+module Combinatorics
+  module Choose
+    include Math
+    #
+    # @param [Fixnum] n The number of elements in the input set
+    #
+    # @param [Fixnum] r Cardinality of subsets to choose
+    #
+    # @raise [RangeError] n must be non-negative
+    #
+    # @raise [RangeError] r must be non-negative
+    #
+    # @raise [RangeError] r must be less than or equal to n
+    #
+    # @return [Fixnum] The binomial coefficient for "n-choose-r"
+    #
+    # @example C(6, 4)
+    #
+    # @see http://en.wikipedia.org/wiki/Binomial_coefficient
+    #
+    # @note This method's naming convention reflects well-known notation used 
+    #       within fields of academic inquiry such as discrete mathematics and
+    #       set theory. It represents a function returning an integer value
+    #       for the cardinality of a k-combination (i.e. binomial coefficient.)
+    #
+    def C(n, r = nil)
+      raise(RangeError, 'n must be non-negative') if n < 0
+
+      if r.nil?
+        factorial(n)
+      else
+        raise(RangeError, 'r must be non-negative') if r < 0
+        raise(RangeError, 'r must be less than or equal to n') if r > n
+        return 1 if n.zero? or n == r
+
+        (n ** r) / factorial(r)
+      end
+    end
+
+    alias cardinality C
+    alias len cardinality
+
+    #
+    # @param [Fixnum] c Input set cardinality
+    #
+    # @return [Array] elements are cardinalities for each subset "1" through "c"
+    #
+    # @raise [RangeError] c must be non-negative
+    #
+    # @example cardinality_all(4)
+    #
+    # @note sum of elements will equal factorial(c)
+    #
+    # @see Combinatorics::Choose::Mixin::cardinality_all
+    # @see http://en.wikipedia.org/wiki/Combinations
+    # 
+    def cardinality_all(c)
+      if c.zero?
+        return []
+      elsif c < 0
+        raise(RangeError, 'c must be non-negative') if c < 0
+      end
+
+      ret = [c]
+
+      2.upto(c) { |x| ret << C(c, x) }
+
+      ret
+    end
+
+    alias C_all cardinality_all
+    alias len_all C_all
+  end
+end
