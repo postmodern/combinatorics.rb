80b02042581319c3f52839f60d1ffa40d9bcd57b
Postmodern <postmodern.mod3@gmail.com>
2011-03-07 21:59:45 -0800

    Removed the executable bit from the benchmark.

--- benchmarks/list_comprehension.rb
+++ benchmarks/list_comprehension.rb
@@ -1,5 +1,3 @@
-#!/usr/bin/env ruby
-
 root_dir = File.expand_path(File.join(File.dirname(__FILE__),'..'))
 lib_dir = File.join(root_dir,'lib')
 $LOAD_PATH << lib_dir unless $LOAD_PATH.include?(lib_dir)
