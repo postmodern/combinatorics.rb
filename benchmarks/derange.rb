dff4d7a71a03c5026f45045cb97d07b786fcaed3
Postmodern <postmodern.mod3@gmail.com>
2011-03-07 22:12:14 -0800

    Added benchmarks for derange.

---
+++ benchmarks/derange.rb
@@ -0,0 +1,18 @@
+#!/usr/bin/env ruby
+
+root_dir = File.expand_path(File.join(File.dirname(__FILE__),'..'))
+lib_dir = File.join(root_dir,'lib')
+$LOAD_PATH << lib_dir unless $LOAD_PATH.include?(lib_dir)
+
+require 'benchmark'
+require 'combinatorics/derange'
+
+Benchmark.bm do |b|
+  (1..10).each do |n|
+    array = (1..n).to_a
+
+    b.report("array (n=#{n})") do
+      array.derange { |s| }
+    end
+  end
+end
