686de1b8699474ca74eb494f2b778811f0cfdfab
Postmodern <postmodern.mod3@gmail.com>
2011-10-09 16:46:28 -0700

    Added missing should asserts to the Choose::Mixin examples.

--- spec/choose/mixin_examples.rb
+++ spec/choose/mixin_examples.rb
@@ -3,33 +3,38 @@ require 'combinatorics/choose'
 
 shared_examples_for 'Choose::Mixin' do
   it 'should return [[]] for [].choose(0).to_a' do
-    set = subject[[]]
+    set     = subject[[]]
+    results = set.choose(0).to_a
 
-    set.choose(0).to_a == [[]]
+    results.should == [[]]
   end
 
   it 'should return [[]] for [1].choose(0).to_a' do
-    set = subject[[1]]
-
-    set.choose(0).to_a == [[]]
+    set     = subject[[1]]
+    results = set.choose(0).to_a
+    
+    results.should == [[]]
   end
 
   it 'should return [[1]] for [1].choose(1).to_a' do
-    set = subject[1]
-
-    set.choose(1).to_a == [[1]]
+    set     = subject[1]
+    results = set.choose(1).to_a
+    
+    results.should == [[1]]
   end
 
   it 'should return [[1], [2]] for [1, 2].choose(1).to_a' do
-    set = subject[[1, 2]]
-
-    set.choose(1).to_a == [[1], [2]]
+    set     = subject[[1, 2]]
+    results = set.choose(1).to_a
+    
+    results.should == [[1], [2]]
   end
 
   it 'should return [[1, 2]] for [1, 2].choose(2).to_a' do
-    set = subject[[1, 2]]
-
-    set.choose(2).to_a == [[1, 2]]
+    set     = subject[[1, 2]]
+    results = set.choose(2).to_a
+    
+    results.should == [[1, 2]]
   end
 
   it 'should alias choose to combos' do
