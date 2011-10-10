123572afd771ce6dcc2a3e076a9ce3feb14d5f0c
Postmodern <postmodern.mod3@gmail.com>
2011-10-09 17:09:50 -0700

    Added more missing should asserts.

--- spec/extensions/math_spec.rb
+++ spec/extensions/math_spec.rb
@@ -4,19 +4,17 @@ require 'combinatorics/extensions/math'
 describe Math do
   describe "sigma" do
     it "should return 6 for sigma(1..3)" do
-      Math.sigma(1..3) == 6
+      Math.sigma(1..3).should == 6
     end
 
     it "should return 60 for sigma(3..5)" do
-      Math.sigma(3..5) == 60
+      Math.sigma(3..5).should == 60
     end
 
     it "should take an optional block argument" do
-      arange = (1..1)
-      aproc  = lambda {|x| x}
-      ares   = Math.sigma(arange, &aproc)
+      result = Math.sigma(1..5) { |i| i * 2 }
 
-      ares.should == 1
+      result.should == (1 * 2) + (2 * 2) + (3 * 2) + (4 * 2) + (5 * 2)
     end
 
     it "should alias Sigma to sigma" do
@@ -26,19 +24,17 @@ describe Math do
 
   describe "pi" do
     it "should return 24 for pi(1..4)" do
-      Math.pi(1..4) == 24
+      Math.pi(1..4).should == 24
     end
 
     it "should return 30 for pi(5..6)" do
-      Math.pi(5..6) == 30
+      Math.pi(5..6).should == 30
     end
 
     it "should take an optional block argument" do
-      arange = 1..1
-      aproc  = lambda {|x| x}
-      ares   = Math.pi(arange, &aproc)
+      result = Math.pi(1..3) { |i| i * 2 }
 
-      ares.should == 1
+      result.should == (1 * 2) * (2 * 2) * (3 * 2)
     end
 
     it "should alias Pi to pi" do
@@ -48,23 +44,23 @@ describe Math do
 
   describe "factorial" do
     it "should return 1 for factorial(0)" do
-      Math.factorial(0) == 1
+      Math.factorial(0).should == 1
     end
 
     it "should return 1 for factorial(1)" do
-      Math.factorial(1) == 1
+      Math.factorial(1).should == 1
     end
 
     it "should return 2 for factorial(2)" do
-      Math.factorial(2) == 2
+      Math.factorial(2).should == 2
     end
 
     it "should return 6 for factorial(3)" do
-      Math.factorial(3) == 6
+      Math.factorial(3).should == 6
     end
     
     it "should return 3628800 for factorial(10)" do
-      Math.factorial(10) == 3628800
+      Math.factorial(10).should == 3628800
     end
 
     it "should raise RangeError for factorial(-1)" do
@@ -74,27 +70,27 @@ describe Math do
 
   describe "subfactorial" do
     it "should return 1 for subfactorial(0)" do
-      Math.subfactorial(0) == 1
+      Math.subfactorial(0).should == 1
     end
 
     it "should return 0 for subfactorial(1)" do
-      Math.subfactorial(1).zero?
+      Math.subfactorial(1).should be_zero
     end
 
     it "should return 1 for subfactorial(2)" do
-      Math.subfactorial(2) == 1
+      Math.subfactorial(2).should == 1
     end
 
     it "should return 2 for subfactorial(3)" do
-      Math.subfactorial(3) == 2
+      Math.subfactorial(3).should == 2
     end
 
     it "should return 9 for subfactorial(4)" do
-      Math.subfactorial(4) == 9
+      Math.subfactorial(4).should == 9
     end
 
     it "should return 44 for subfactorial(5)" do
-      Math.subfactorial(5) == 4
+      Math.subfactorial(5).should == 4
     end
 
     it "should raise RangeError for subfactorial(-1)" do
