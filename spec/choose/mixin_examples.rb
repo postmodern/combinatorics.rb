ed0000b0ebaf7f1b2cb0bd86333caaac1f135c26
Postmodern <postmodern.mod3@gmail.com>
2011-10-10 21:21:29 -0700

    Fixed the Choose specs, was a simple typo with Array[] / Set[] syntax.

--- spec/choose/mixin_examples.rb
+++ spec/choose/mixin_examples.rb
@@ -3,14 +3,14 @@ require 'combinatorics/choose'
 
 shared_examples_for "Choose::Mixin" do
   it "should return [[]] for [].choose(0).to_a" do
-    set     = subject[[]]
+    set     = subject[]
     results = set.choose(0).to_a
 
     results.should == [[]]
   end
 
   it "should return [[]] for [1].choose(0).to_a" do
-    set     = subject[[1]]
+    set     = subject[1]
     results = set.choose(0).to_a
     
     results.should == [[]]
@@ -24,14 +24,14 @@ shared_examples_for "Choose::Mixin" do
   end
 
   it "should return [[1], [2]] for [1, 2].choose(1).to_a" do
-    set     = subject[[1, 2]]
+    set     = subject[1, 2]
     results = set.choose(1).to_a
     
     results.should == [[1], [2]]
   end
 
   it "should return [[1, 2]] for [1, 2].choose(2).to_a" do
-    set     = subject[[1, 2]]
+    set     = subject[1, 2]
     results = set.choose(2).to_a
     
     results.should == [[1, 2]]
